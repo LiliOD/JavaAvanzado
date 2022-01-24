2017f280090eff66e6ebbe7186ec604b11810f28
LiliOD <96136589+LiliOD@users.noreply.github.com>
2022-01-24 12:29:42 -0600

    Ejercicios git NPM

---
+++ NPM-Practice-master/get_users.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+async function makeGetRequest() {
+  
+
+  let res = await axios.get('http://localhost:3000/users');
+  
+
+  let data = res.data;
+  console.log(data);
+}
+
+
+makeGetRequest();
